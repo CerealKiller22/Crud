69825d9a95cb37a25f5b95b8e4bbedf850ba1937
CerealKiller22 <dariorestrepo208618@correo.itm.edu.co>
2021-02-28 22:13:52 -0500

    Add Task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,66 @@
-import React from 'react'
+import { isEmpty } from 'lodash'
+import React, {useState} from 'react'
+import shortid from 'shortid'
 
 function App() {
+  const [task, setTask] = useState("")
+  const [tasks, setTasks] = useState([])
+  const addTask = (e) => {
+    e.preventDefault()    //evita recargar la página por el boton submit
+    if(isEmpty(task)){
+      console.log("Task Empty")
+      return
+    }
+
+
+    const newTask = {
+      id:shortid.generate(),           //Genera un codigo alfanumero sin repetir
+      name: task
+    }
+
+    setTasks([...tasks, newTask])    
+    setTask("")
+  }
+
   return (
     <div className="container mt-5">
       <h1>Tareas</h1>
       <hr/>
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">Lista de tareas</h4>
+          <ul className="list-group">            
+            {
+              tasks.map((task) => (
+                <li className="list-group-item" key={task.id}>
+                  <span className="lead">{task.name}</span>
+                  <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+                  <button className="btn btn-warning btn-sm float-right">Editar</button>
+                </li>
+              ))              
+            }            
+          </ul>
+        </div>
+
+        <div className="col-4">
+          <h4 className="text-center">Form</h4>
+          <form onSubmit={addTask}>
+            <input
+              type="text"
+              className="form-control mb-2"
+              placeholder="ingrese la tarea..."
+              onChange={(text) => setTask(text.target.value)}  //Obtiene el valor ingresado en el input
+              value = {task}
+            />
+            <button 
+              class="btn btn-dark btn-block"
+              type="submit"
+            >              
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
